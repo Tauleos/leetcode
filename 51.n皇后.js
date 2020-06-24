3129628b5a8580bfcb704e89bad07b11858782b8
Tauleos <847301609@qq.com>
2020-06-24 18:19:54 +0800

    feat: 51增加注释

--- 51.n皇后.js
+++ 51.n皇后.js
@@ -60,32 +60,36 @@
 var solveNQueens = function (n) {
 	const res = [];
 	function backtrack(board, row) {
+		//触发结束条件
 		if (row === n) {
-			res.push(board.map(c=>'.'.repeat(c)+'Q'+'.'.repeat(n-c-1)));
+			res.push(board.map((c) => '.'.repeat(c) + 'Q' + '.'.repeat(n - c - 1)));
 			return;
 		}
 		for (let col = 0; col < n; col++) {
+			//去除不合理选项
 			if (!isValid(board, row, col)) {
 				continue;
 			}
+			//做决策
 			board.push(col);
+			//进入下一层决策树,在这里意味着去棋盘的下一行
 			backtrack(board, row + 1);
+			//撤销决策
 			board.pop();
 		}
 	}
 	function isValid(board, row, col) {
-		
 		for (let i = 0; i < row; i++) {
 			//列查询
 			if (board[i] === col) {
 				return false;
 			}
 			// 左上角跟右下角差值相等   1,1 2,2 3,3
-			if(row-col === i-board[i]){
+			if (row - col === i - board[i]) {
 				return false;
 			}
 			//左下角跟右上角和值相等    3,1 2,2 1,3
-			if(row+col === i+board[i]){
+			if (row + col === i + board[i]) {
 				return false;
 			}
 		}
