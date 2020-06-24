2186f40bedd57f6b7fde98c5001a0ef539e3c4d3
Tauleos <847301609@qq.com>
2020-06-24 18:17:27 +0800

    feat: finish 51

--- 51.n皇后.js
+++ 51.n皇后.js
@@ -14,42 +14,42 @@
  * Testcase Example:  '4'
  *
  * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
- * 
- * 
- * 
+ *
+ *
+ *
  * 上图为 8 皇后问题的一种解法。
- * 
+ *
  * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
- * 
+ *
  * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
- * 
+ *
  * 示例:
- * 
+ *
  * 输入: 4
  * 输出: [
  * ⁠[".Q..",  // 解法 1
  * ⁠ "...Q",
  * ⁠ "Q...",
  * ⁠ "..Q."],
- * 
+ *
  * ⁠["..Q.",  // 解法 2
  * ⁠ "Q...",
  * ⁠ "...Q",
  * ⁠ ".Q.."]
  * ]
  * 解释: 4 皇后问题存在两个不同的解法。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
- * 
+ *
+ *
+ *
  * 皇后，是国际象棋中的棋子，意味着国王的妻子。皇后只做一件事，那就是“吃子”。当她遇见可以吃的棋子时，就迅速冲上去吃掉棋子。当然，她横、竖、斜都可走一到七步，可进可退。（引用自
  * 百度百科 - 皇后 ）
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -57,8 +57,42 @@
  * @param {number} n
  * @return {string[][]}
  */
-var solveNQueens = function(n) {
-
+var solveNQueens = function (n) {
+	const res = [];
+	function backtrack(board, row) {
+		if (row === n) {
+			res.push(board.map(c=>'.'.repeat(c)+'Q'+'.'.repeat(n-c-1)));
+			return;
+		}
+		for (let col = 0; col < n; col++) {
+			if (!isValid(board, row, col)) {
+				continue;
+			}
+			board.push(col);
+			backtrack(board, row + 1);
+			board.pop();
+		}
+	}
+	function isValid(board, row, col) {
+		
+		for (let i = 0; i < row; i++) {
+			//列查询
+			if (board[i] === col) {
+				return false;
+			}
+			// 左上角跟右下角差值相等   1,1 2,2 3,3
+			if(row-col === i-board[i]){
+				return false;
+			}
+			//左下角跟右上角和值相等    3,1 2,2 1,3
+			if(row+col === i+board[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+	backtrack([], 0);
+	return res;
 };
+console.log('result', solveNQueens(4));
 // @lc code=end
-
