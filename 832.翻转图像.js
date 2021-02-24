2224d65c1ae962d66b25b4ff6831ad9b0bb409c0
Tauleos <847301609@qq.com>
2021-02-24 17:40:00 +0800

    832

---
+++ 832.翻转图像.js
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=832 lang=javascript
+ *
+ * [832] 翻转图像
+ *
+ * https://leetcode-cn.com/problems/flipping-an-image/description/
+ *
+ * algorithms
+ * Easy (75.50%)
+ * Likes:    234
+ * Dislikes: 0
+ * Total Accepted:    68.1K
+ * Total Submissions: 85.8K
+ * Testcase Example:  '[[1,1,0],[1,0,1],[0,0,0]]'
+ *
+ * 给定一个二进制矩阵 A，我们想先水平翻转图像，然后反转图像并返回结果。
+ * 
+ * 水平翻转图片就是将图片的每一行都进行翻转，即逆序。例如，水平翻转 [1, 1, 0] 的结果是 [0, 1, 1]。
+ * 
+ * 反转图片的意思是图片中的 0 全部被 1 替换， 1 全部被 0 替换。例如，反转 [0, 1, 1] 的结果是 [1, 0, 0]。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：[[1,1,0],[1,0,1],[0,0,0]]
+ * 输出：[[1,0,0],[0,1,0],[1,1,1]]
+ * 解释：首先翻转每一行: [[0,1,1],[1,0,1],[0,0,0]]；
+ * ⁠    然后反转图片: [[1,0,0],[0,1,0],[1,1,1]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：[[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+ * 输出：[[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ * 解释：首先翻转每一行: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]]；
+ * ⁠    然后反转图片: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(A) {
+  let reversed = A.map(i=>i.reverse());
+  let result = [];
+  reversed.forEach((i,index)=>{
+    result[index] = i.map(item=>item^1)
+  })
+  // console.log(result);
+  return result;
+    
+
+};
+console.log(flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]]))
+// @lc code=end
+
