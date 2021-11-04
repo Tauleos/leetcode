778486a7854902d98adafaa03589021d804c2bbc
Tauleos <847301609@qq.com>
2021-11-04 10:55:56 +0800

    finish:daily

---
+++ 367.有效的完全平方数.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=367 lang=javascript
+ *
+ * [367] 有效的完全平方数
+ *
+ * https://leetcode-cn.com/problems/valid-perfect-square/description/
+ *
+ * algorithms
+ * Easy (43.90%)
+ * Likes:    270
+ * Dislikes: 0
+ * Total Accepted:    92.5K
+ * Total Submissions: 207.8K
+ * Testcase Example:  '16'
+ *
+ * 给定一个 正整数 num ，编写一个函数，如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+ *
+ * 进阶：不要 使用任何内置的库函数，如  sqrt 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：num = 16
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：num = 14
+ * 输出：false
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+	let base = 1;
+	let square = base * base;
+	while (square < num) {
+		base++;
+		square = base * base;
+	}
+	return square == num;
+};
+// @lc code=end
